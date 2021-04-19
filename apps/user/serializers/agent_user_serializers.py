058e340fc2a3c8bc118d0fe19d233c848cf0d4b6
bishnu <sbishnu019@gmail.com>
2021-04-19 11:34:22 +0545

    fix(User): Fix list-agent-user and list-portal-user end-point

--- apps/user/serializers/agent_user_serializers.py
+++ apps/user/serializers/agent_user_serializers.py
@@ -1,5 +1,16 @@
-from apps.user.serializers.base_serializers import UserDetailSerializer
+from rest_framework import serializers
+
+from apps.user.models import AgentUser
 
 
-class ListAgentUserSerializer(UserDetailSerializer):
-    pass
+class AgentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AgentUser
+        fields = '__all__'
+
+
+class ListAgentUserSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    email = serializers.EmailField(source='user.email')
--- apps/user/serializers/portal_user_serializers.py
+++ apps/user/serializers/portal_user_serializers.py
@@ -1,5 +1,16 @@
-from apps.user.serializers.base_serializers import UserDetailSerializer
+from rest_framework import serializers
+
+from apps.user.models import PortalUser
 
 
-class ListPortalUserSerializer(UserDetailSerializer):
-    pass
+class PortalUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PortalUser
+        fields = '__all__'
+
+
+class ListPortalUserSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    email = serializers.EmailField(source='user.email')
