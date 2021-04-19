2c17176e5f9b234280778ee00a9578495f703e0f
bishnu <sbishnu019@gmail.com>
2021-04-19 12:54:24 +0545

    fix(Question): Fix list-question end-point

--- apps/question/serializers/question_serializers.py
+++ apps/question/serializers/question_serializers.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from apps.question.models import Question
@@ -24,15 +25,16 @@ class AddQuestionSerializer(QuestionSerializer):
 
 
 class ListQuestionSerializer(AddQuestionSerializer):
-    question_options = serializers.ListSerializer(
-        child=serializers.CharField(),
-        source='questionoption_set'
-    )
+    question_options = serializers.SerializerMethodField()
 
     class Meta(AddQuestionSerializer.Meta):
         fields = AddQuestionSerializer.Meta.fields + (
             'id',
         )
+
+    @swagger_serializer_method(serializer_or_field=serializers.ListSerializer(child=serializers.CharField()))
+    def get_question_options(self, instance):
+        return instance.questionoption_set.values_list('option', flat=True)
 
 
 class QuestionDetailSerializer(ListQuestionSerializer):
