4f15cc6beda108a87f88421cd74a8f446e973466
bishnu <sbishnu019@gmail.com>
2021-05-17 20:34:30 +0545

    fix(Config): Increase Page_size to 500

--- config/settings/base.py
+++ config/settings/base.py
@@ -246,7 +246,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 500,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'EXCEPTION_HANDLER': 'apps.core.exceptions.custom_exception_handler',
     'DEFAULT_RENDERER_CLASSES': [
