d6cbfceed90af45d6f96207d322637bb16dc39b2
bishnu <sbishnu019@gmail.com>
2021-05-25 11:55:34 +0545

    fix(Localize): fix end-point for list-country

--- apps/localize/serializers/country_serializers.py
+++ apps/localize/serializers/country_serializers.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
 from apps.core.serializers import IdNameSerializer
@@ -13,13 +14,13 @@ class CountrySerializer(serializers.ModelSerializer):
 class AddCountrySerializer(CountrySerializer):
     class Meta(CountrySerializer.Meta):
         fields = (
-            'name', 
+            'name',
             'region'
         )
 
 
 class ListCountrySerializer(CountrySerializer):
-    city = IdNameSerializer(many=True, source='city_set')
+    city = serializers.SerializerMethodField()
 
     class Meta(CountrySerializer.Meta):
         fields = (
@@ -27,6 +28,15 @@ class ListCountrySerializer(CountrySerializer):
             'name',
             'city'
         )
+
+    @swagger_serializer_method(serializer_or_field=IdNameSerializer())
+    def get_city(self, instance):
+        query_params = self.context['request'].query_params
+        if query_params.get('questionnaire', None):
+            city = instance.city_set.filter(questionnaire__id=query_params.get('questionnaire'))
+        else:
+            city = instance.city_set.all()
+        return IdNameSerializer(city, many=True).data
 
 
 class ListNationalitySerializer(CountrySerializer):
