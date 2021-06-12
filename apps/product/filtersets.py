35c8fe10e371fd5d194415ee74c1fa785d889d4b
bishnu <sbishnu019@gmail.com>
2021-06-12 10:40:05 +0545

    fix(Product): Add multiple brand filter in sku

--- apps/product/filtersets.py
+++ apps/product/filtersets.py
@@ -1,11 +1,18 @@
 from django_filters import rest_framework as filters
 
+from apps.core.filtersets import IdInFilter
 from apps.product.models import SKU, Brand
 
 
 class SKUFilter(filters.FilterSet):
     category = filters.CharFilter(
         field_name='brand__category'
+    )
+
+    brand = IdInFilter(
+        field_name='brand',
+        lookup_expr='in',
+        label='brand'
     )
 
     class Meta:
