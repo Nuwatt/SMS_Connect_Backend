ce9b12bd20158a23fe6febf9a950d432b3b5fa4e
123bmc <binshimanayil@gmail.com>
2021-07-12 11:47:58 +0400

    Added Ultra Mobile App download url.

--- config/urls.py
+++ config/urls.py
@@ -12,7 +12,7 @@ from drf_yasg.views import get_schema_view
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicons/favicon.ico'))),
-    path('appdownload/',RedirectView.as_view(url='https://sms-connect-dp.s3.ap-south-1.amazonaws.com/media/appdownload/SMSConnect0.9.apk'))
+    path('appdownload/',RedirectView.as_view(url='https://sms-connect-dp.s3.ap-south-1.amazonaws.com/media/appdownload/SmsconnectUltra.apk'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
