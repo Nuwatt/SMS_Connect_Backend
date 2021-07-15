d1e022b3509b0707c2a7ebd27aec1f6dd720ffe7
bishnu <sbishnu019@gmail.com>
2021-07-15 11:56:41 +0545

    fix(Core): Set Timezone to Asia/Riyadh

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ DEBUG = env.bool("DJANGO_DEBUG", default=False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Riyadh"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -34,7 +34,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
 USE_L10N = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
-# USE_TZ = True
+USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
 LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 
