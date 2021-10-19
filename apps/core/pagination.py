497be0fe521f22913da3350f988f7a939033d0e7
bishnu <sbishnu019@gmail.com>
2021-10-19 21:04:11 +0545

    fix(Core): Fix ReportPagination

--- apps/core/pagination.py
+++ apps/core/pagination.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from rest_framework.pagination import CursorPagination
+from rest_framework.pagination import CursorPagination, LimitOffsetPagination
 from rest_framework.response import Response
 
 
@@ -7,8 +7,8 @@ class CustomCursorPagination(CursorPagination):
     page_size_query_param = 'limit'
 
 
-class ReportPagination(CustomCursorPagination):
-    page_size = settings.REPORTING_MAX_LIMIT
+class ReportPagination(LimitOffsetPagination):
+    default_limit = settings.REPORTING_MAX_LIMIT
 
     def get_paginated_response(self, data):
         return Response(data)
