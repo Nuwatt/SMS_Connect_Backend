86a4307458ba55d7596dd18d99f8499b32ffef95
bishnu <sbishnu019@gmail.com>
2021-12-15 17:56:25 +0545

    fix(Snap): Fix Admin site

--- apps/snap/admin.py
+++ apps/snap/admin.py
@@ -4,6 +4,15 @@ from apps.core.admin import BaseModelAdmin
 from apps.snap import models
 
 
+class SnapModelAdmin(BaseModelAdmin):
+    list_display = BaseModelAdmin.list_display + (
+        'name',
+    )
+    search_fields = BaseModelAdmin.search_fields + (
+        'name',
+    )
+
+
 @admin.register(models.PriceMonitorSnap)
 class PriceMonitorSnapAdmin(BaseModelAdmin):
     list_display = BaseModelAdmin.list_display + (
@@ -75,30 +84,30 @@ class ConsumerSnapAdmin(BaseModelAdmin):
 
 
 @admin.register(models.SnapChannel)
-class SnapChannelAdmin(BaseModelAdmin):
+class SnapChannelAdmin(SnapModelAdmin):
     pass
 
 
 @admin.register(models.SnapRetailer)
-class SnapRetailerAdmin(BaseModelAdmin):
+class SnapRetailerAdmin(SnapModelAdmin):
     pass
 
 
 @admin.register(models.SnapStore)
-class SnapStoreAdmin(BaseModelAdmin):
+class SnapStoreAdmin(SnapModelAdmin):
     pass
 
 
 @admin.register(models.SnapCategory)
-class SnapCategoryAdmin(BaseModelAdmin):
+class SnapCategoryAdmin(SnapModelAdmin):
     pass
 
 
 @admin.register(models.SnapBrand)
-class SnapBrandAdmin(BaseModelAdmin):
+class SnapBrandAdmin(SnapModelAdmin):
     pass
 
 
 @admin.register(models.SnapSKU)
-class SnapSKUAdmin(BaseModelAdmin):
+class SnapSKUAdmin(SnapModelAdmin):
     pass
