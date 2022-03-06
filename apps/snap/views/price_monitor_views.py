7121a18a0894cff0e9dee4e31a4802c0d6094993
123bmc <binshimanayil@gmail.com>
2022-03-06 14:38:58 +0400

    tesy

--- apps/snap/views/price_monitor_views.py
+++ apps/snap/views/price_monitor_views.py
@@ -115,7 +115,7 @@ class MonthMinPriceMonitorSnapReportView(BaseReportView):
     filterset_class = PriceMonitorSnapFilter
 
     def get_queryset(self):
-        print(self.request.GET['sku'],"---------------------------")
+        print(self.request.GET.dict()['sku'],"---------------------------")
         data = price_monitor_usecases.MonthMinPriceMonitorSnapReportUseCase().execute()
 
 
